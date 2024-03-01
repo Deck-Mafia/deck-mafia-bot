8d732380d634bba2e44decf0b1780604f44487e5
CybeastID <57740797+CybeastID@users.noreply.github.com>
2024-03-01 12:11:40 -0500

    Update Peek and Request.ts
    Independent developer created commands

--- src/deckmafia/commands/peek.ts
+++ src/deckmafia/commands/peek.ts
@@ -1,70 +1,124 @@
-import { ChatInputCommandInteraction, CommandInteraction, SlashCommandBuilder } from 'discord.js';
+import { ActionRowBuilder, ButtonBuilder, ChatInputCommandInteraction, SlashCommandBuilder, ButtonStyle } from 'discord.js';
 import { prisma } from '../..';
-import { newSlashCommand, SlashCommand } from '../../structures/SlashCommand';
-import string from 'string-similarity';
+import { newSlashCommand } from '../../structures/SlashCommand';
+
+const cardsPerPage = 25;
 
 const c = new SlashCommandBuilder();
 c.setName('peek');
 c.setDescription('See what cards a player has in their inventory.');
 c.addUserOption((i) => i.setName('user').setDescription('User you want to add a card for').setRequired(true));
 
-async function getAllCardNames() {
-	const cards = await prisma.card.findMany({
-		where: {
-			isPublic: true,
-		},
-		select: {
-			name: true,
-		},
-	});
-
-	let cardNames: string[] = [];
-	cards.forEach((card) => cardNames.push(card.name));
-	return cardNames;
-}
-
-async function getClosestCardName(cardName: string, list: string[]) {
-	console.log(cardName, list);
-	const result = string.findBestMatch(cardName, list);
-	return result;
-}
-
 export default newSlashCommand({
-	data: c,
-	async execute(i: ChatInputCommandInteraction) {
-		const user = i.options.getUser('user', true);
+    data: c,
+    async execute(i: ChatInputCommandInteraction) {
+        const user = i.options.getUser('user', true);
 
-		try {
-			const inventory = await prisma.inventory.findUnique({
-				where: {
-					discordId: user.id,
-				},
-				include: {
-					ownedCards: {
-						include: {
-							card: true,
-						},
-					},
-				},
-			});
+        try {
+            const inventory = await prisma.inventory.findUnique({
+                where: {
+                    discordId: user.id,
+                },
+                include: {
+                    ownedCards: {
+                        include: {
+                            card: true,
+                        },
+                    },
+                },
+            });
 
-			if (!inventory) return i.reply({ content: 'User does not have an inventory. To make one, use the `/give` command.' });
+            if (!inventory) return i.reply({ content: 'User does not have an inventory. To make one, use the `/give` command.' });
 
-			let value = `\`\`\`diff\nINVENTORY FOR ${user.username.toUpperCase()}\n- ${inventory.ownedCards.length} CARDS TOTAL\n\n`;
+            let page = 0;
 
-			inventory.ownedCards.forEach((ownedCard) => {
-				if (ownedCard.card) value += `+ ${ownedCard.card.name}\n`;
-			});
+            const initialReply = async () => {
+                const start = page * cardsPerPage;
+                const end = start + cardsPerPage;
+                const slicedCards = inventory.ownedCards.slice(start, end);
 
-			value += '```';
+                let value = `\`\`\`diff\nINVENTORY FOR ${user.username.toUpperCase()}\n- ${inventory.ownedCards.length} CARDS TOTAL\n\n`;
 
-			await i.reply({ content: value });
-		} catch (err) {
-			await i.reply({
-				ephemeral: true,
-				content: 'An unexpected error has occurred when fetching this card',
-			});
-			console.error(err);
-		}
-	},
+                const cardCounts: { [cardName: string]: number } = {};
+
+                slicedCards.forEach((ownedCard) => {
+                    if (ownedCard.card) {
+                        const cardName = ownedCard.card.name;
+                        cardCounts[cardName] = (cardCounts[cardName] || 0) + 1;
+                    }
+                });
+
+                Object.entries(cardCounts).forEach(([cardName, count]) => {
+                    value += `+ ${cardName} x${count}\n`;
+                });
+
+                value += `\nPage ${page + 1} of ${Math.ceil(inventory.ownedCards.length / cardsPerPage)}\n\`\`\``;
+
+                const buttonRow = new ActionRowBuilder<ButtonBuilder>().setComponents(
+                    new ButtonBuilder().setCustomId('peek-prev-page').setLabel('Previous Page').setStyle(ButtonStyle.Secondary).setDisabled(page === 0),
+                    new ButtonBuilder().setCustomId('peek-next-page').setLabel('Next Page').setStyle(ButtonStyle.Secondary).setDisabled(page === Math.floor(inventory.ownedCards.length / cardsPerPage))
+                );
+
+                const reply = await i.reply({ content: value, components: [buttonRow.toJSON()], ephemeral: true });
+                return reply;
+            };
+
+            const initialReplyInteraction = await initialReply();
+
+            const collector = i.channel?.createMessageComponentCollector({
+                filter: (interaction) => (interaction.isButton() && interaction.customId.startsWith('peek')) && interaction.user.id === i.user.id,
+                time: 60000,
+            });
+
+            collector?.on('collect', async (interaction) => {
+                if (interaction.customId === 'peek-prev-page' || interaction.customId === 'peek-next-page') {
+                    if (interaction.customId === 'peek-prev-page') {
+                        await interaction.deferUpdate();
+                        page = Math.max(0, page - 1);
+                    } else if (interaction.customId === 'peek-next-page') {
+                        await interaction.deferUpdate();
+                        page = Math.min(Math.floor(inventory.ownedCards.length / cardsPerPage), page + 1);
+                    }
+
+                    const start = page * cardsPerPage;
+                    const end = start + cardsPerPage;
+                    const slicedCards = inventory.ownedCards.slice(start, end);
+
+                    let value = `\`\`\`diff\nINVENTORY FOR ${user.username.toUpperCase()}\n- ${inventory.ownedCards.length} CARDS TOTAL\n\n`;
+
+                    const cardCounts: { [cardName: string]: number } = {};
+
+                    slicedCards.forEach((ownedCard) => {
+                        if (ownedCard.card) {
+                            const cardName = ownedCard.card.name;
+                            cardCounts[cardName] = (cardCounts[cardName] || 0) + 1;
+                        }
+                    });
+
+                    Object.entries(cardCounts).forEach(([cardName, count]) => {
+                        value += `+ ${cardName} x${count}\n`;
+                    });
+
+                    value += `\nPage ${page + 1} of ${Math.ceil(inventory.ownedCards.length / cardsPerPage)}\n\`\`\``;
+
+                    const updatedButtonRow = new ActionRowBuilder<ButtonBuilder>().setComponents(
+                        new ButtonBuilder().setCustomId('peek-prev-page').setLabel('Previous Page').setStyle(ButtonStyle.Secondary).setDisabled(page === 0),
+                        new ButtonBuilder().setCustomId('peek-next-page').setLabel('Next Page').setStyle(ButtonStyle.Secondary).setDisabled(page === Math.floor(inventory.ownedCards.length / cardsPerPage))
+                    );
+
+                    await initialReplyInteraction.edit({ content: value, components: [updatedButtonRow.toJSON()] });
+                }
+            });
+
+            collector?.on('end', async () => {
+                await initialReplyInteraction.edit({ content: "`Buttons expired! Please use the command again!`", components: [] });
+            });
+        } catch (err) {
+            await i.reply({
+                ephemeral: true,
+                content: 'An unexpected error has occurred while running this command. Please contact tech support.',
+            });
+            console.error(err);
+        }
+    },
 });

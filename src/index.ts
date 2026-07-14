2f4b8948296dbab38649d2b31ac440563bf5fade
CybeastID <paul.crispin@rutgers.edu>
2026-07-14 16:54:06 -0400

    removes startup caller

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,11 @@ import { PrismaClient } from '@prisma/client';
 import '@total-typescript/ts-reset';
 import { start as startDeckMafia } from './clients/deckmafia';
 import { start as startdeiMilites } from './clients/deimilites';
-import { cleanup } from './cleanupActionEvents';
 
 export const prisma = new PrismaClient();
 export const database = prisma;
 
 (async () => {
-	// One-time cleanup: purge years of stale ActionEvents and dead VoteCounts.
-	// TODO: Remove this call after it runs successfully once.
-	await cleanup(prisma);
-
 	startDeckMafia();
 //	startdeiMilites();
 })();
